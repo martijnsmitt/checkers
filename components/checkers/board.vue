5826fc9667096ed4767ee5c77d43fd0df8f601e1
martijnsmitt <martijnsmitt@gmail.com>
2022-08-17 15:23:46 +0200

    add attacks

--- components/checkers/board.vue
+++ components/checkers/board.vue
@@ -1,16 +1,21 @@
 <script setup>
 const { board, boardLayout, playerA, playerB, sync, turns } = useDatabase()
 
-// const currentPlayer = playerA
+const currentPlayer = playerA
+
+const finishTurn = () => {
+  // @TO-DO: check if new location should give a double stone
+  turns.value++
+  sync()
+}
 
 const move = (oldLocation, newLocation) => {
   board.value[newLocation].player = board.value[oldLocation].player
   board.value[oldLocation].player = 0
   selectedStone.value = null
-  // check if new location should give a double stone
-  turns.value++
-  sync()
 }
+
+const remove = (location) => board.value[location].player = 0
 
 const isSimpleMove = (key) => {
 
@@ -46,10 +51,98 @@ const isSimpleMove = (key) => {
   return false
 }
 
+// Area's on board with different rules
+// -----------------
+// | A |   B   | C |
+// -----------------
+// |   |       |   |
+// | D |   E   | F |
+// |   |       |   |
+// -----------------
+// | G |   H   | I |
+// -----------------
+const getArea = (key, row, column) => {
+  if (row >= 9) {
+    if (column === 1) {
+      return 'A'
+    } else if (column <= 4) {
+      return 'B'
+    } else {
+      return 'C'
+    }
+  } else if (row >= 3) {
+    if (column === 1) {
+      return 'D'
+    } else if (column <= 4) {
+      return 'E'
+    } else {
+      return 'F'
+    }
+  } else {
+    if (column === 1) {
+      return 'G'
+    } else if (column <= 4) {
+      return 'H'
+    } else {
+      return 'I'
+    }
+  }
+}
+
+const testAttack = (key, attacking, dest) => {
+  // within reach of board
+  if (dest > 50 || dest <= 0) return
+
+  // is destination free
+  if (board.value[dest].player) return
+
+  // are you attacking anyone
+  if (!board.value[attacking].player) return
+
+  // are you attacking yourself
+  if (board.value[attacking].player === board.value[key].player) return
+
+  // you must attack!
+  return { key, attacking, dest }
+}
+
+const attackTopLeft = (key, type) => testAttack(key, key + (type ? 4 : 5), key + 9)
+const attackTopRight = (key, type) => testAttack(key, key + (type ? 5 : 6), key + 11)
+const attackBottomRight = (key, type) => testAttack(key, key - (type ? 5 : 4), key - 9)
+const attackBottomLeft = (key, type) => testAttack(key, key - (type ? 6 : 5), key - 11)
+
+const possibleAttacks = (key) => {
+  const column = (key % 5 || 5)
+  const row = Math.ceil((key) / 5)
+  const type = row % 2
+
+  switch(getArea(key, row, column)) {
+    case 'A':
+      return [ attackBottomRight(key, type) ]
+    case 'B':
+      return [ attackBottomRight(key, type), attackBottomLeft(key, type) ]
+    case 'C':
+      return [ attackBottomLeft(key, type) ]
+    case 'D':
+      return [ attackTopRight(key, type), attackBottomRight(key, type) ]
+    case 'E':
+      return [ attackTopLeft(key, type), attackTopRight(key, type), attackBottomRight(key, type), attackBottomLeft(key, type) ]
+    case 'F':
+      return [ attackTopLeft(key, type), attackBottomLeft(key, type) ]
+    case 'G':
+      return [ attackTopRight(key, type) ]
+    case 'H':
+      return [ attackTopLeft(key, type), attackTopRight(key, type) ]
+    case 'I':
+      return [ attackTopLeft(key, type) ]
+  }
+  console.error(`switch with area's should be triggered`)
+  return 
+}
+
 const selectedStone = ref(null)
 
 const select = (key) => {
-
   // is it your turn?
   // if (player)
 
@@ -81,6 +174,21 @@ const select = (key) => {
     return
   }
 
+  // should the player attack
+  const attack = possibleAttacks(selectedStone.value).filter(e => e)
+  if (attack.length) {
+    console.log({ attack })
+    const isAttacking = attack.filter(a => a.dest === key)[0]
+    if (!isAttacking) {
+      console.warn('Player must attack!')
+      return
+    }
+    console.log({isAttacking})
+    move(selectedStone.value, key)
+    remove(isAttacking.attacking)
+    return finishTurn()
+  }
+
   // is new location taken?
   if (board.value[key].player) {
     console.warn('cannot move stone on another stone')
@@ -90,13 +198,10 @@ const select = (key) => {
   // check if simple move
   if (isSimpleMove(key)) {
     move(selectedStone.value, key)
-    return
+    return finishTurn()
   }
 
-
-// check if strike move
-  // move(selectedStone.value, key)
-  console.log('TO-DO: ',key)
+  console.log('@TO-DO: catch this step?: ',key)
 }
 
 </script>
